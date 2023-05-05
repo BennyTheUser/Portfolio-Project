7e97c587af690fbbfbcc13be3888fb2a98fbe77c
BennyTheUser <aaron.xiong@student.cart.org>
2023-05-05 10:39:00 -0700

    Portfolio Progress 11
    Aligned images to the center on the home page and started work on the slideshow.

---
+++ script.js
@@ -0,0 +1,11 @@
+let slideIndex = 1;
+    showSlides(slideIndex);
+
+function nextSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
